7bda4ca4a07b514525257e6f10925c69239a7a1e
januarpancaran <januar352@gmail.com>
2025-01-19 19:41:52 +0700

    adding some new options

--- lua/options.lua
+++ lua/options.lua
@@ -1,16 +1,30 @@
 vim.o.number = true
 vim.o.relativenumber = true
 
+vim.o.scrolloff = 10
 vim.o.signcolumn = "yes"
 
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+vim.o.expandtab = true
+
+vim.o.smartindent = true
+
+vim.o.hlsearch = false
+vim.o.incsearch = true
 
 vim.o.updatetime = 300
 vim.o.termguicolors = true
+
 
 vim.o.clipboard = "unnamedplus"
 
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
+
+vim.keymap.set("n", "j", "jzz")
+vim.keymap.set("n", "k", "kzz")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
