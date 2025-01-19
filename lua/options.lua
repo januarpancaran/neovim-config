6ca88ccd22f6c123d096bd26e8d1c114d075a948
januarpancaran <januar352@gmail.com>
2025-01-19 22:22:39 +0700

    adding plugin for tmux

--- lua/options.lua
+++ lua/options.lua
@@ -17,6 +17,7 @@ vim.o.incsearch = true
 vim.o.updatetime = 300
 vim.o.termguicolors = true
 
+vim.o.swapfile = false
 
 vim.o.clipboard = "unnamedplus"
 
@@ -25,6 +26,13 @@ vim.keymap.set("n", "<leader>pp", vim.cmd.Ex)
 
 vim.keymap.set("n", "j", "jzz")
 vim.keymap.set("n", "k", "kzz")
+vim.keymap.set("n", "{", "{zz")
+vim.keymap.set("n", "}", "}zz")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-h>", "wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", "wincmd j<CR>")
+vim.keymap.set("n", "<C-k>", "wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", "wincmd l<CR>")
--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -5,7 +5,7 @@ return {
 			"hrsh7th/cmp-nvim-lsp",
 			"L3MON4D3/LuaSnip",
 			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+			"rafamadriz/friendly-snippets"
 		},
 		config = function()
 			local cmp = require("cmp")
@@ -17,13 +17,13 @@ return {
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 				}, {
-					{ name = "buffer" },
+						{ name = "buffer" },
 				}),
 
 				snippet = {
 					expand = function(args)
 						luasnip.lsp_expand(args.body)
-					end,
+					end
 				},
 
 				window = {
@@ -32,25 +32,25 @@ return {
 				},
 
 				mapping = cmp.mapping.preset.insert({
-					["<C-n>"] = cmp.mapping.select_next_item(),
-					["<C-p>"] = cmp.mapping.select_prev_item(),
-					["<C-d>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<CR>"] = cmp.mapping.confirm({
+					[ "<C-n>" ] = cmp.mapping.select_next_item(),
+					[ "<C-p>" ] = cmp.mapping.select_prev_item(),
+					[ "<C-d>" ] = cmp.mapping.scroll_docs(-4),
+					[ "<C-f>" ] = cmp.mapping.scroll_docs(4),
+					[ "<C-Space>" ] = cmp.mapping.complete(),
+					[ "<CR>" ] = cmp.mapping.confirm({
 						behavior = cmp.ConfirmBehavior.Replace,
 						select = true,
 					}),
-					["<Tab>"] = cmp.mapping(function(fallback)
+					[ "<Tab>" ] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
-						elseif luasnip.expand_or_locally_jumpable() then
+						elseif luasnip.expand_or_locally_jumpable() then 
 							luasnip.expand_or_jump()
 						else
 							fallback()
 						end
 					end, { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(function(fallback)
+					[ "<S-Tab>" ] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
 						elseif luasnip.locally_jumpable(-1) then
@@ -58,9 +58,9 @@ return {
 						else
 							fallback()
 						end
-					end, { "i", "s" }),
-				}),
+					end, { "i", "s" })
+				})
 			})
-		end,
-	},
+		end
+	}
 }
