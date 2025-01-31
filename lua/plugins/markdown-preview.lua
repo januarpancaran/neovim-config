7b7b5d4f27628b0b999c1fa4092d6caa888f7d5d
januarpancaran <januar352@gmail.com>
2025-01-31 23:04:17 +0700

    fixing indentation

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,13 +1,13 @@
 return {
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		build = "cd app && npm install",
-		init = function()
-			vim.g.mkdp_filetypes = { "markdown" }
-		end,
-		ft = { "markdown" },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
 
-		vim.keymap.set("n", "<C-m>", "<CMD>MarkdownPreview<CR>", {}),
-	},
+    vim.keymap.set("n", "<C-m>", "<CMD>MarkdownPreview<CR>", {}),
+  },
 }
