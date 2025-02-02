6243a962cd5c4469331c6565bac759244612fb6e
januarpancaran <januar352@gmail.com>
2025-02-02 14:27:33 +0700

    changing vuels to volar

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
-    end
+    end,
   },
 
   {
@@ -27,11 +27,11 @@ return {
           "svelte",
           "tailwindcss",
           "ts_ls",
-          "vuels",
+          "volar",
         },
       })
-    end
-  }, 
+    end,
+  },
 
   {
     "neovim/nvim-lspconfig",
@@ -39,27 +39,27 @@ return {
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      lspconfig.bashls.setup( { capabilities = capabilities })
-      lspconfig.clangd.setup( { capabilities = capabilities })
-      lspconfig.cssls.setup( { capabilities = capabilities })
-      lspconfig.eslint.setup( { capabilities = capabilities })
-      lspconfig.gopls.setup( { capabilities = capabilities })
-      lspconfig.html.setup( { capabilities = capabilities })
-      lspconfig.jdtls.setup( { capabilities = capabilities })
-      lspconfig.jsonls.setup( { capabilities = capabilities })
-      lspconfig.lua_ls.setup( { capabilities = capabilities })
-      lspconfig.marksman.setup( { capabilities = capabilities })
-      lspconfig.pyright.setup( { capabilities = capabilities })
-      lspconfig.rust_analyzer.setup( { capabilities = capabilities })
-      lspconfig.sqls.setup( { capabilities = capabilities })
-      lspconfig.svelte.setup( { capabilities = capabilities })
-      lspconfig.tailwindcss.setup( { capabilities = capabilities })
-      lspconfig.ts_ls.setup( { capabilities = capabilities })
-      lspconfig.vuels.setup( { capabilities = capabilities })
+      lspconfig.bashls.setup({ capabilities = capabilities })
+      lspconfig.clangd.setup({ capabilities = capabilities })
+      lspconfig.cssls.setup({ capabilities = capabilities })
+      lspconfig.eslint.setup({ capabilities = capabilities })
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      lspconfig.html.setup({ capabilities = capabilities })
+      lspconfig.jdtls.setup({ capabilities = capabilities })
+      lspconfig.jsonls.setup({ capabilities = capabilities })
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.marksman.setup({ capabilities = capabilities })
+      lspconfig.pyright.setup({ capabilities = capabilities })
+      lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+      lspconfig.sqls.setup({ capabilities = capabilities })
+      lspconfig.svelte.setup({ capabilities = capabilities })
+      lspconfig.tailwindcss.setup({ capabilities = capabilities })
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
+      lspconfig.volar.setup({ capabilities = capabilities })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end
-  }
+    end,
+  },
 }
