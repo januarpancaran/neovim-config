11e815012a7a0d67c2c53b3d694fe2818af0f475
januarpancaran <januar352@gmail.com>
2025-02-09 21:00:21 +0700

    removing neo-tree

--- init.lua
+++ init.lua
@@ -1,2 +1,3 @@
 require("config.options")
+require("config.remaps")
 require("config.lazy")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,6 +10,7 @@ vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 vim.o.smartindent = true
+vim.o.autoindent = true
 
 vim.o.hlsearch = false
 vim.o.incsearch = true
@@ -20,18 +21,3 @@ vim.o.termguicolors = true
 vim.o.swapfile = false
 
 vim.o.clipboard = "unnamedplus"
-
-vim.g.mapleader = " "
-
-vim.keymap.set("n", "j", "jzz")
-vim.keymap.set("n", "k", "kzz")
-vim.keymap.set("n", "{", "{zz")
-vim.keymap.set("n", "}", "}zz")
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "<C-h>", "wincmd h<CR>")
-vim.keymap.set("n", "<C-j>", "wincmd j<CR>")
-vim.keymap.set("n", "<C-k>", "wincmd k<CR>")
-vim.keymap.set("n", "<C-l>", "wincmd l<CR>")
