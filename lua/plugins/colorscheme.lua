b4d0baa6355e4f1a0be1dc1b472ae30977d1feb7
januarpancaran <januar352@gmail.com>
2026-02-22 17:31:23 +0700

    feat: changing theme to onedark

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
-	"folke/tokyonight.nvim",
+	"olimorris/onedarkpro.nvim",
 	priority = 1000,
 	lazy = false,
 	config = function()
-		vim.cmd.colorscheme("tokyonight-night")
+		vim.cmd.colorscheme("onedark")
 	end,
 }
