e4585b2449e6301aca9d4f052670c831fa674595
januarpancaran <januar352@gmail.com>
2026-03-04 08:34:40 +0700

    feat: adding show hidden files option to oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -11,6 +11,9 @@ return {
 				default_file_explorer = true,
 				delete_to_trash = true,
 				skip_confirm_for_simple_edits = true,
+				view_options = {
+					show_hidden = true,
+				},
 			})
 		end,
 	},
