27da4b216ae60f1f67e22f58f94df7a6a67ee3db
januarpancaran <januar352@gmail.com>
2026-04-03 23:03:22 +0700

    feat: changing snacks explorer to oil

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,15 @@
+return {
+  { "nvim-mini/mini.pairs", enabled = false },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false },
+    },
+    keys = {
+      { "<leader>fe", false },
+      { "<leader>fE", false },
+      { "<leader>E", false },
+      { "<leader>e", false },
+    },
+  },
+}
---
+++ lua/plugins/oil.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    lazy = false,
+    key = {
+      vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open oil" }),
+    },
+    config = function()
+      require("oil").setup({
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+        },
+      })
+    end,
+  },
+}
