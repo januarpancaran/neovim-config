7e5afb7857871795477af0727308b1d6f3d17411
januarpancaran <januar352@gmail.com>
2026-04-03 22:52:16 +0700

    feat: adding my keymaps and options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "j", "jzz")
+vim.keymap.set("n", "k", "kzz")
+vim.keymap.set("n", "{", "{zz")
+vim.keymap.set("n", "}", "}zz")
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-h>", "wincmd h<CR>")
+vim.keymap.set("n", "<C-j>", "wincmd j<CR>")
+vim.keymap.set("n", "<C-k>", "wincmd k<CR>")
+vim.keymap.set("n", "<C-l>", "wincmd l<CR>")
